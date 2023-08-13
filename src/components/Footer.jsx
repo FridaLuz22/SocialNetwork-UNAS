d99d0bd945415f30342bf676955bcef8709b396a
Frida Fernandez <fridaluzf14@gmail.com>
2023-08-13 18:22:15 -0500

    Avance del navbar y footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+
+
+function Footer(){
+  return(
+    <footer>
+      <div className="card-footer"> 
+        <a href="">
+          <img src="../public/assets/icon_logo.png" alt="" />
+        </a>
+        <a href="">
+          <img src="../public/assets/icon_video.png" alt="" />
+        </a>
+        <a href="">
+          <img src="../public/assets/icon_more.png" alt="" />
+        </a>
+        <a href="">
+          <img src="../public/assets/icon_map.png" alt="" />
+        </a>
+        <a href="">
+          <img src="../public/assets/cuenta_icon.png" alt="" />
+        </a>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+
+
+function Header(){
+  return(
+    <header className="">
+      <div className="card-header">
+        <img src="../public/assets/LETRAS RED UNAS.png" alt="" />
+        <div>
+          <a href="">
+            <img src="../public/assets/buscador.png" alt="" />
+          </a>
+          <a href="">
+            <img src="../public/assets/NOTIFICATION.png" alt="" />
+          </a>
+          <a href="">
+            <img src="../public/assets/message.png" alt="" />
+          </a>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header;
