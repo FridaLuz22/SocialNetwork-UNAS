4bbeb7e5d38c2ee394a495072ad61c1adfdf912b
jecnerNS <21jeiner@gmail.com>
2023-08-24 15:54:55 -0500

    nueva actualizacion de login y home

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Header(){
+    return(
+      <header className="">
+        <div className="flex items-center justify-between px-4 pt-6 ">
+          <h1>holaa</h1>
+        </div>
+      </header>
+    )
+  }
+  
+  export default Header;
