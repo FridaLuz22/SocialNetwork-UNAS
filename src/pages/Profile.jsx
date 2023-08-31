d7e5f9ecf73078306b609623cbdb58a0af33d49d
Frida Fernandez <fridaluzf14@gmail.com>
2023-08-31 18:05:56 -0500

    Add Profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,33 @@
-// import { Link } from "react-router-dom";
+import Footer from '../components/Footer';
 
 function Profile(){
-    return(
+  return (
+    <>
       <header className="">
-        <div className="flex items-center justify-between px-4 pt-6 ">
-          <h1>holaa</h1>
+        <a href="">
+          <img src="../assets/icons/flecha.png" alt="" />
+        </a>
+        <div className="flex items-center justify-between px-4 pt-8 ">
+        <h2>Perfil</h2>
+        <div className="flex items-center gap-6">
+            <a href="">
+              <img src="../assets/icons/editar.png" alt="" />
+            </a>
+            <a href="">
+              <img src="../assets/icons/message.png" alt="" />
+            </a>
+          </div>
         </div>
-      </header>
-    )
+      </header>  
+      <div className="flex items-center gap-8">
+            <a href="">
+              <img src="../assets/icons/usuario_avatar.png" alt="" />
+            </a>
+            <h2>Nombre usuario</h2>
+      </div>
+      <Footer />
+    </>
+  );
   }
   
   export default Profile;
