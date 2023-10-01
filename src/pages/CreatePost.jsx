b2ec47434f15d342a035c5c12fea9984390ad149
jecnerNS <21jeiner@gmail.com>
2023-09-30 21:24:26 -0500

    Vista crear publicacion y login mejorado x2

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 
 function CreatePost() {
   const containerStyle = {
-    minHeight: '100vh', // Ocupa toda la altura de la ventana
-    backgroundColor: 'white', // Color de fondo opcional
+    minHeight: '100vh', //  altura de la ventana
+    backgroundColor: 'white', // Color de fondo 
   };
 
   return (
     <div style={containerStyle} className="flex justify-center items-center">
       <div className="border border-gray-300 p-4 rounded w-full max-w-screen-md">
-        {/* Primera fila */}
+        {/* 1*/}
         <div className="border-b border-gray-300 mb-4 pb-4">
           <header className="flex items-center justify-between">
             <div className="flex items-center">
               <button className="flex items-center text-black">
                 <img
-                  src="../assets/icons/FlechaIzquierda.png"  // Ruta de la imagen de tu botón
+                  src="../assets/icons/FlechaIzquierda.png"  // Ruta 
                   alt="Botón de Imagen"
                   className="w-6 h-6 mr-2 border-transparent  hover:bg-transparent  rounded focus:outline-none focus:ring focus:ring-transparent active:shadow-md active:bg-transparent transform transition-transform hover:-translate-y-1 active:-translate-y-0.5 "  // Clases para establecer el tamaño de la imagen
                 />
@@ -27,7 +27,7 @@ function CreatePost() {
           </header>
         </div>
 
-        {/* Segunda fila */}
+        {/* 2 */}
         <div className="border-b mb-4 pb-4  border-transparent  hover:bg-transparent text-black font-semibold py-2 px-4 rounded focus:outline-none focus:ring focus:ring-transparent active:shadow-md active:bg-transparent transform transition-transform hover:-translate-y-1 active:-translate-y-0.5">
           <button className="text-black font-bold flex items-center">
             <img src="../assets/icons/PerfilUsuario.png" alt="Cámara" className="w-8 h-8 mr-2  " />
@@ -35,15 +35,15 @@ function CreatePost() {
           </button>
         </div>
 
-        {/* Tercera fila */}
+        {/* 3 */}
         <div className="border-b border-gray-300 mb-4 pb-4">
           <textarea
-            className="w-full border text-black bg-stone-200 border-gray-300 p-2 h-59" // Cambia la clase aquí
+            className="w-full border text-black bg-stone-200 border-gray-300 p-2 h-59" 
             placeholder="Escribe tu publicación aquí"
           ></textarea>
         </div>
 
-        {/* Cuarta fila */}
+        {/* 4*/}
         <div className="border-b border-gray-300 mb-4 pb-4 hover:bg-stone-200 text-black font-semibold py-2 px-4 rounded focus:outline-none focus:ring focus:ring-stone-200 active:shadow-md active:bg-stone-100 transform transition-transform hover:-translate-y-1 active:-translate-y-0.5 ">
 
           <button className="text-black flex items-center">
@@ -53,7 +53,7 @@ function CreatePost() {
         </div>
 
 
-        {/* Quinta fila */}
+        {/* 5*/}
         
         <div className="border-b border-gray-300 mb-4 pb-4 hover:bg-stone-200 text-black font-semibold py-2 px-4 rounded focus:outline-none focus:ring focus:ring-stone-200 active:shadow-md active:bg-stone-100 transform transition-transform hover:-translate-y-1 active:-translate-y-0.5">
           <button className="text-black flex items-center">
@@ -62,7 +62,7 @@ function CreatePost() {
           </button>
         </div>
 
-        {/* Sexta fila */}
+        {/*6 */}
         
         <div className="border-b border-gray-300 mb-4 pb-4 hover:bg-stone-200 text-black font-semibold py-2 px-4 rounded focus:outline-none focus:ring focus:ring-stone-200 active:shadow-md active:bg-stone-100 transform transition-transform hover:-translate-y-1 active:-translate-y-0.5">
           <button className="text-black flex items-center">
@@ -72,7 +72,7 @@ function CreatePost() {
         </div>
         
 
-        {/* Séptima fila */}
+        {/* 7*/}
         <div className="border-b border-gray-300 mb-4 pb-4 hover:bg-stone-200 text-black font-semibold py-2 px-4 rounded focus:outline-none focus:ring focus:ring-stone-200 active:shadow-md active:bg-stone-100 transform transition-transform hover:-translate-y-1 active:-translate-y-0.5">
           <button className="text-black flex items-center">
             <img src="../assets/icons/EnVivo.png" alt="Cámara" className="w-4 h-4 mr-2" />
